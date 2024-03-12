7dcaa0e8308479211ab216b3fac3d3969379fc4f
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-03-12 14:19:35 +0100

    Refactor import statements and set sameSite attribute for cookies

--- src/hooks/UseAuth.jsx
+++ src/hooks/UseAuth.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { axiosJson } from "../api/axiosConfig";
-import Cookies from 'js-cookie'; 
+import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import { useSetCurrentUser } from "../contexts/CurrentUserContext";
 
@@ -10,28 +10,35 @@ export function useAuth() {
 
   const setCookies = (accessToken, refreshToken) => {
     const secure = window.location.protocol.includes("https");
-    Cookies.set("access", accessToken, { path: "/", sameSite: "None", secure });
-    Cookies.set("refresh", refreshToken, { path: "/token/refresh/", sameSite: "None", secure });
+    Cookies.set("access", accessToken, { path: "/", sameSite: "Lax", secure });
+    Cookies.set("refresh", refreshToken, {
+      path: "/token/refresh/",
+      sameSite: "Lax",
+      secure
+    });
   };
 
   // Register a new user
   async function register(username, email, password) {
     try {
-      const response = await axiosJson.post("/users/register/", { username, email, password });
-  
+      const response = await axiosJson.post("/users/register/", {
+        username,
+        email,
+        password
+      });
+
       if (response.status === 200 || response.status === 201) {
         const { access, refresh } = response.data;
-        
+
         if (access && refresh) {
           setCookies(access, refresh);
           setCurrentUser({ isLoggedIn: true });
-          
+
           useEffect(() => {
             if (currentUser?.isLoggedIn) {
               navigate("/dashboard");
             }
           }, [currentUser, navigate]);
-        
         } else {
           console.error("Registration successful, but no tokens received.");
         }
@@ -41,23 +48,22 @@ export function useAuth() {
     }
   }
 
-
-
-
   // Login an existing user
   async function login(username, password) {
     try {
-      const response = await axiosJson.post("/users/login/", { username, password });
+      const response = await axiosJson.post("/users/login/", {
+        username,
+        password
+      });
       if (response.status === 200) {
-        const { access, refresh, user_id } = response.data; 
+        const { access, refresh, user_id } = response.data;
         setCookies(access, refresh);
-        
-        setCurrentUser({ isLoggedIn: true, userId: user_id }); 
+
+        setCurrentUser({ isLoggedIn: true, userId: user_id });
         navigate("/dashboard");
       }
     } catch (error) {
       console.error("Login failed:", error);
-      
     }
   }
   // Logout current user, backend will revoke the refresh token
